20a680287fae708bea564570881f3811164cf7da
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-27 22:57:15 +0330

    fix(ui): fix footer to the bottom

--- google-scholar/src/pages/Layout/Layout.tsx
+++ google-scholar/src/pages/Layout/Layout.tsx
@@ -5,7 +5,7 @@ import Footer from "../../components/Footer";
 
 const Layout = () => {
   return (
-    <body>
+    <body className="page-container">
       <Header />
       <Outlet />
       <Footer />
