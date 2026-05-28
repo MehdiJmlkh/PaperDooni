43cb9e51dd6948ae97c279a3cd20c5d48c19ed7e
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-28 19:25:47 +0330

    feat(ui): add icon to citation card

--- google-scholar/src/components/CitationCard/CitationCard.tsx
+++ google-scholar/src/components/CitationCard/CitationCard.tsx
@@ -1,9 +1,11 @@
 import Card from "../Card";
 import "./CitationCard.css";
+import { VscReferences } from "react-icons/vsc";
 
 const CitationCard = () => {
   return (
     <Card className="citation-card">
+        <VscReferences className="citation-card__icon" size={30} />
         <span className="citation-card__title">Title</span>
     </Card>
   );
