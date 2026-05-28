7d67f5ad9cfd0f3c8518f6471ca5d3cd609f97c9
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-28 19:26:18 +0330

    feat(ui): add ArticlePage

--- google-scholar/src/routes.tsx
+++ google-scholar/src/routes.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
+import ArticlePage from "./pages/ArticlePage";
 
 const router = createBrowserRouter([
   {
     element: <Layout />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "articles/:id", element: <ArticlePage /> },
+    ],
   },
 ]);
 
