c8f8a800ccef9881a793010d932dc4cad9d3e08a
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-29 12:00:32 +0330

    feat(ui): add SimpleCard component

---
+++ google-scholar/src/components/SimpleCard/SimpleCard.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import "./SimpleCard.css";
+
+interface Props {
+  children: ReactNode;
+  className?: string;
+}
+
+const SimpleCard = ({ children, className }: Props) => {
+  return <div className={`my-card ${className}`}>{children}</div>;
+};
+
+export default SimpleCard;
--- google-scholar/src/pages/ArticlePage/ArticlePage.tsx
+++ google-scholar/src/pages/ArticlePage/ArticlePage.tsx
@@ -1,64 +1,72 @@
 import CitationCard from "../../components/CitationCard";
+import SimpleCard from "../../components/SimpleCard";
 import "./ArticlePage.css";
 
 const ArticlePage = () => {
   return (
     <main className="article-page">
-      <h1 className="article-page__title">Title</h1>
-      <div>2026</div>
-      <h2 className="article-page__heading">Abstract</h2>
-      <p className="article-page__content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe facere
-        voluptatum, officia recusandae impedit qui maiores voluptate earum vel
-        esse libero asperiores ipsam corrupti vitae voluptas. Excepturi quisquam
-        facilis rerum officiis doloremque optio quia, reprehenderit voluptas
-        architecto expedita non commodi, laborum iste, aut eius delectus eveniet
-        ipsum fuga laboriosam voluptates ullam. Aperiam assumenda quas, non
-        incidunt quia neque ipsam pariatur laborum dolores nulla fuga veritatis
-        maiores rem. Perferendis, maiores non? Reiciendis neque doloribus vitae
-        voluptas officiis placeat architecto mollitia, tempore fugiat
-        consequatur laudantium nihil repellat dignissimos, hic corporis modi
-        similique porro? Sunt fuga quis quia voluptatibus quos quaerat explicabo
-        quisquam velit necessitatibus sint voluptas optio fugit ab harum quam,
-        labore vel. Ad optio, fugit odio adipisci odit ducimus assumenda id,
-        quos dolore autem totam necessitatibus, molestias vitae nisi. Nihil
-        itaque dicta atque fugiat eos blanditiis, dolores cumque praesentium
-        laudantium accusantium veritatis rem illum possimus a quas nostrum eum
-        expedita illo aut voluptatum assumenda ducimus! In quod vel mollitia et
-        quaerat reprehenderit ipsum facilis consequuntur natus, ut atque
-        distinctio? Tempora reprehenderit adipisci, ducimus provident distinctio
-        quas modi qui. Voluptate dignissimos fugiat labore hic et illum aliquam
-        quidem aut culpa expedita! Adipisci repudiandae quo nisi doloribus,
-        nostrum iste, ad, nesciunt obcaecati labore maiores amet deserunt
-        voluptatem placeat consequuntur? Est, recusandae vitae? Eius quia
-        doloremque inventore, doloribus incidunt quae fugiat porro tempora
-        itaque impedit sapiente quisquam tenetur ex delectus vitae in quam enim
-      </p>
-      <h2 className="article-page__heading">Body</h2>
-      <p className="article-page__content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-        voluptatum suscipit, quis a itaque numquam commodi corrupti aspernatur,
-        tempore quo assumenda amet quisquam harum autem asperiores dolores minus
-        labore rerum ea unde fuga vel soluta voluptas. Officiis sit saepe
-        praesentium vitae illo eum voluptates dicta autem quam possimus animi
-        libero, pariatur quod labore ratione incidunt. Repellendus deserunt
-        voluptatum culpa quos delectus, velit hic sequi optio vel sit nihil unde
-        magnam pariatur fugiat perferendis ipsum animi sunt harum ex veniam
-        inventore eius error iste? Quidem fugit nam, ipsum ullam incidunt
-        voluptate expedita, aliquid corrupti, quas quae ut consequuntur impedit
-        temporibus? Quaerat mollitia obcaecati blanditiis vitae saepe quis
-        pariatur consequatur amet quod itaque laboriosam magnam ea, odit
-        accusamus quisquam aliquid perferendis quo rerum corporis expedita sequi
-        repellat eius perspiciatis. A quos modi ullam, assumenda exercitationem
-        nisi atque suscipit adipisci molestias mollitia obcaecati odio in ipsa
-        incidunt cupiditate fugiat recusandae dicta optio, commodi vel autem
-        fugit saepe. Laboriosam perferendis recusandae officia soluta, labore
-        deserunt quos cumque magni sunt, nisi, aliquid esse ipsum aperiam
-        consequatur animi rerum iusto repudiandae harum dolores? Ratione ex id
-        ea vel recusandae asperiores reiciendis dolorem deleniti, nam veritatis
-        eos accusamus sapiente laborum culpa architecto ut est dolor laboriosam
-        maxime.
-      </p>
+      <SimpleCard className="article-page__header">
+        <h1 className="article-page__title">Title</h1>
+        <span className="article-page__year">2026</span>
+      </SimpleCard>
+      <SimpleCard>
+        <h2 className="article-page__heading">Abstract</h2>
+        <p className="article-page__content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe facere
+          voluptatum, officia recusandae impedit qui maiores voluptate earum vel
+          esse libero asperiores ipsam corrupti vitae voluptas. Excepturi
+          quisquam facilis rerum officiis doloremque optio quia, reprehenderit
+          voluptas architecto expedita non commodi, laborum iste, aut eius
+          delectus eveniet ipsum fuga laboriosam voluptates ullam. Aperiam
+          assumenda quas, non incidunt quia neque ipsam pariatur laborum dolores
+          nulla fuga veritatis maiores rem. Perferendis, maiores non? Reiciendis
+          neque doloribus vitae voluptas officiis placeat architecto mollitia,
+          tempore fugiat consequatur laudantium nihil repellat dignissimos, hic
+          corporis modi similique porro? Sunt fuga quis quia voluptatibus quos
+          quaerat explicabo quisquam velit necessitatibus sint voluptas optio
+          fugit ab harum quam, labore vel. Ad optio, fugit odio adipisci odit
+          ducimus assumenda id, quos dolore autem totam necessitatibus,
+          molestias vitae nisi. Nihil itaque dicta atque fugiat eos blanditiis,
+          dolores cumque praesentium laudantium accusantium veritatis rem illum
+          possimus a quas nostrum eum expedita illo aut voluptatum assumenda
+          ducimus! In quod vel mollitia et quaerat reprehenderit ipsum facilis
+          consequuntur natus, ut atque distinctio? Tempora reprehenderit
+          adipisci, ducimus provident distinctio quas modi qui. Voluptate
+          dignissimos fugiat labore hic et illum aliquam quidem aut culpa
+          expedita! Adipisci repudiandae quo nisi doloribus, nostrum iste, ad,
+          nesciunt obcaecati labore maiores amet deserunt voluptatem placeat
+          consequuntur? Est, recusandae vitae? Eius quia doloremque inventore,
+          doloribus incidunt quae fugiat porro tempora itaque impedit sapiente
+          quisquam tenetur ex delectus vitae in quam enim
+        </p>
+      </SimpleCard>
+      <SimpleCard>
+        <h2 className="article-page__heading">Body</h2>
+        <p className="article-page__content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          voluptatum suscipit, quis a itaque numquam commodi corrupti
+          aspernatur, tempore quo assumenda amet quisquam harum autem asperiores
+          dolores minus labore rerum ea unde fuga vel soluta voluptas. Officiis
+          sit saepe praesentium vitae illo eum voluptates dicta autem quam
+          possimus animi libero, pariatur quod labore ratione incidunt.
+          Repellendus deserunt voluptatum culpa quos delectus, velit hic sequi
+          optio vel sit nihil unde magnam pariatur fugiat perferendis ipsum
+          animi sunt harum ex veniam inventore eius error iste? Quidem fugit
+          nam, ipsum ullam incidunt voluptate expedita, aliquid corrupti, quas
+          quae ut consequuntur impedit temporibus? Quaerat mollitia obcaecati
+          blanditiis vitae saepe quis pariatur consequatur amet quod itaque
+          laboriosam magnam ea, odit accusamus quisquam aliquid perferendis quo
+          rerum corporis expedita sequi repellat eius perspiciatis. A quos modi
+          ullam, assumenda exercitationem nisi atque suscipit adipisci molestias
+          mollitia obcaecati odio in ipsa incidunt cupiditate fugiat recusandae
+          dicta optio, commodi vel autem fugit saepe. Laboriosam perferendis
+          recusandae officia soluta, labore deserunt quos cumque magni sunt,
+          nisi, aliquid esse ipsum aperiam consequatur animi rerum iusto
+          repudiandae harum dolores? Ratione ex id ea vel recusandae asperiores
+          reiciendis dolorem deleniti, nam veritatis eos accusamus sapiente
+          laborum culpa architecto ut est dolor laboriosam maxime.
+        </p>
+      </SimpleCard>
       <h2 className="article-page__heading">Citations</h2>
       <CitationCard />
       <CitationCard />
