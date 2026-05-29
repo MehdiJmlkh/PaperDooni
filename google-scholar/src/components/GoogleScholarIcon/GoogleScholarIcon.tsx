83f096e04bedcfedfe1ee0b16739276b8286e7d6
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-29 10:35:38 +0330

    feat(ui): add navigation link to google scholar icon

--- google-scholar/src/components/GoogleScholarIcon/GoogleScholarIcon.tsx
+++ google-scholar/src/components/GoogleScholarIcon/GoogleScholarIcon.tsx
@@ -1,10 +1,17 @@
-import './GoogleScholarIcon.css'
-import Icon from '../../assets/icon.png'
+import "./GoogleScholarIcon.css";
+import Icon from "../../assets/icon.png";
+import { useNavigate } from "react-router-dom";
 
 const GoogleScholarIcon = () => {
+  const navigate = useNavigate();
   return (
-    <img className="google-scholar-icon" src={Icon} alt="" />
-  )
-}
+    <img
+      className="google-scholar-icon"
+      src={Icon}
+      alt=""
+      onClick={() => navigate("/")}
+    />
+  );
+};
 
-export default GoogleScholarIcon
+export default GoogleScholarIcon;
