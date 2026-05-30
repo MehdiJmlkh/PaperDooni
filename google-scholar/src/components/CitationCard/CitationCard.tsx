46c30d81adc70a8ca9d4e17ec4653531369063ee
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-30 20:50:56 +0330

    feat(ui): add navigation to article page to CitationCard

--- google-scholar/src/components/CitationCard/CitationCard.tsx
+++ google-scholar/src/components/CitationCard/CitationCard.tsx
@@ -1,15 +1,20 @@
+import { VscReferences } from "react-icons/vsc";
+import { useNavigate } from "react-router-dom";
 import { Citation } from "../../services/articleService";
 import AnimatedCard from "../AnimatedCard";
 import "./CitationCard.css";
-import { VscReferences } from "react-icons/vsc";
 
 interface Props {
   citation: Citation;
 }
 
 const CitationCard = ({ citation }: Props) => {
+  const navigate = useNavigate();
   return (
-    <AnimatedCard className="citation-card">
+    <AnimatedCard
+      className="citation-card"
+      onClick={() => navigate(`/articles/${citation.id}`)}
+    >
       <VscReferences className="citation-card__icon" size={30} />
       <span className="citation-card__title">{citation?.title}</span>
     </AnimatedCard>
