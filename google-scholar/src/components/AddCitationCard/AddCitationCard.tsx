c6d61d85475c1d30494e9b44b64e4ba060314b37
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-30 18:29:19 +0330

    feat(query): fetch all articles in AddCitationCard

--- google-scholar/src/components/AddCitationCard/AddCitationCard.tsx
+++ google-scholar/src/components/AddCitationCard/AddCitationCard.tsx
@@ -4,21 +4,22 @@ import Pagination from "../Pagination";
 import SearchBar from "../SearchBar";
 import SimpleCard from "../SimpleCard";
 import "./AddCitationCard.css";
+import { useArticles } from "../../queries/useArticles";
 
 const AddCitationCard = () => {
   const [page, setPage] = useState(1);
+  const pageSize = 4;
+  const { data: articlePage } = useArticles(page, pageSize);
   return (
     <SimpleCard>
       <div className="add-citation__heading">Citations</div>
       <SearchBar />
       <div className="add-citation__articles">
-        <CheckBox>Title</CheckBox>
-        <CheckBox>Title</CheckBox>
-        <CheckBox>Title</CheckBox>
-        <CheckBox>Title</CheckBox>
-
+        {articlePage?.content.map((article) => (
+          <CheckBox>{article.title}</CheckBox>
+        ))}
         <Pagination
-          totalPages={5}
+          totalPages={Math.ceil((articlePage?.total || 1) / pageSize)}
           pageNumber={page}
           onClick={(next) => setPage(next)}
         />
