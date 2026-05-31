f1144444eebb2842ed0e8d6afa5711fdda97515c
Mehdi <mehdijmlkh1382@gmail.com>
2026-05-31 10:35:53 +0330

    feat(query): invalidate articles key in cache after adding a new article

--- google-scholar/src/queries/useAddArticle.ts
+++ google-scholar/src/queries/useAddArticle.ts
@@ -1,11 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import articleService, {
   AddArticleRequest,
   ArticleSummary,
 } from "../services/articleService";
 
 export const useAddArticle = () => {
+  const queryClient = useQueryClient();
   return useMutation<ArticleSummary, Error, AddArticleRequest>({
     mutationFn: articleService.addArticle,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["articles"],
+      });
+    },
   });
 };
