ec796fb712bae27cd87867424a1bf16c8077ccf0
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-16 13:37:28 +0330

    feat(api): add User entity

---
+++ src/main/java/ir/ac/ut/ece/ie/users/User.java
@@ -0,0 +1,24 @@
+package ir.ac.ut.ece.ie.users;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+}
