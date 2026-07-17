0b626cb845d2daad2e027c0535b86b69de6f051e
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-17 18:10:31 +0330

    fix(api): permit signing up without authentication

--- src/main/java/ir/ac/ut/ece/ie/config/SecurityConfig.java
+++ src/main/java/ir/ac/ut/ece/ie/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import ir.ac.ut.ece.ie.auth.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,6 +38,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(c ->
                 c
                     .requestMatchers("/auth/**").permitAll()
+                    .requestMatchers(HttpMethod.POST,"/users").permitAll()
                     .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
