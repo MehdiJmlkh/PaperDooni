a9ba78a98f9ba6196fc035a69cb687e20dc72bbc
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-19 10:38:00 +0330

    feat(query): send login request

--- google-scholar/src/pages/SignInPage/SignInPage.tsx
+++ google-scholar/src/pages/SignInPage/SignInPage.tsx
@@ -1,12 +1,13 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import Button from "../../components/Button";
 import Form from "../../components/Form";
 import Input from "../../components/Input";
 import Link from "../../components/Link";
-import { date, z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
 import PasswordInput from "../../components/PasswordInput/PasswordInput";
+import { useLogin } from "../../queries/useLogin";
 import "./SignInPage.css";
-import { useForm } from "react-hook-form";
 
 const schema = z.object({
   username: z.string().min(1),
@@ -24,8 +25,10 @@ const SignInPage = () => {
     resolver: zodResolver(schema),
   });
 
+  const login = useLogin();
+
   return (
-    <Form onSubmit={handleSubmit((data) => console.log(data))}>
+    <Form onSubmit={handleSubmit((data) => login.mutate(data))}>
       <h1 className="form__heading">Sign in</h1>
       <Input {...register("username")} placeholder="Username" />
       <PasswordInput {...register("password")} placeholder="Password" />
