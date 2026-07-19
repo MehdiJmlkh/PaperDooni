5c2e995b365ab6d86318e548cce69ff9d8acf421
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-19 21:46:59 +0330

    feat(query): send edit email request

--- google-scholar/src/components/EditEmailModal/EditEmailModal.tsx
+++ google-scholar/src/components/EditEmailModal/EditEmailModal.tsx
@@ -1,18 +1,52 @@
+import { z } from "zod";
 import Button from "../Button";
 import Input from "../Input";
 import Modal from "../Modal";
 import "./EditEmailModal.css";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEditEmail } from "../../queries/useEditEmail";
 
 interface Props {
   show: boolean;
   onClose: () => void;
 }
 
+const schema = z.object({
+  newEmail: z.string().email("Email must be valid").or(z.literal("")),
+});
+
+type FormData = z.infer<typeof schema>;
+
 const EditEmailModal = ({ show, onClose }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onSubmit",
+    reValidateMode: "onChange",
+  });
+
+  const canSubmit = watch("newEmail")?.trim() !== "";
+
+  const editEmail = useEditEmail();
+
   return (
     <Modal title="Edit Email" show={show} onClose={onClose}>
-      <Input placeholder="New Email" />
-      <Button className="">Submit</Button>
+      <Input
+        {...register("newEmail")}
+        placeholder="New Email"
+        error={errors.newEmail?.message}
+      />
+      <Button
+        disable={!canSubmit}
+        onClick={handleSubmit((data) => editEmail.mutate(data))}
+      >
+        Submit
+      </Button>
     </Modal>
   );
 };
