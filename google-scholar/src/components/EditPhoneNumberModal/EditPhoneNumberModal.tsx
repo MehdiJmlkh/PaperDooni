70c181bb20b31168f183f68d85a7a5142bc1b496
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-19 23:37:23 +0330

    feat(query): send edit phone number request

--- google-scholar/src/components/EditPhoneNumberModal/EditPhoneNumberModal.tsx
+++ google-scholar/src/components/EditPhoneNumberModal/EditPhoneNumberModal.tsx
@@ -1,19 +1,32 @@
 import "./EditPhoneNumberModal.css";
-import Modal from "../Modal";
-import Input from "../Input";
-import Button from "../Button";
+import EditModal from "../EditModal";
+import { useEditPhoneNumber } from "../../queries/useEditPhoneNumber";
+import { z } from "zod";
 
 interface Props {
   show: boolean;
   onClose: () => void;
 }
 
+const schema = z.object({
+  newPhoneNumber: z
+    .string()
+    .regex(/^0/, "Phone number must start with 0")
+    .length(11, "Phone number must contain 11 digits")
+    .or(z.literal("")),
+});
+
 const EditPhoneNumberModal = ({ show, onClose }: Props) => {
   return (
-    <Modal title="Edit Phone Number" show={show} onClose={onClose}>
-      <Input placeholder="New Phone Number" />
-      <Button className="">Submit</Button>
-    </Modal>
+    <EditModal
+      show={show}
+      onClose={onClose}
+      title="Edit Phone Number"
+      fieldName="newPhoneNumber"
+      placeholder="New Phone Number"
+      schema={schema}
+      mutation={useEditPhoneNumber()}
+    />
   );
 };
 
