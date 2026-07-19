a8c2a9822688d69ac5fc8d7bb99944196d1f7c4c
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-19 13:41:40 +0330

    feat(query): add access token to all requests

--- google-scholar/src/services/apiClient.ts
+++ google-scholar/src/services/apiClient.ts
@@ -1,5 +1,18 @@
 import axios from "axios";
 
-export default axios.create({
+const apiClient = axios.create({
   baseURL: "http://localhost:8080",
 });
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+export default apiClient;
