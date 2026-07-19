f5e76431f0f92815c462f0d49c610fafe2f7b5ff
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-19 14:16:25 +0330

    feat(query): refresh access token on getting 401 response

--- google-scholar/src/services/apiClient.ts
+++ google-scholar/src/services/apiClient.ts
@@ -15,4 +15,38 @@ apiClient.interceptors.request.use(
   (error) => Promise.reject(error),
 );
 
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (originalRequest.url === "/auth/refresh") {
+      return Promise.reject(error);
+    }
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const response = await apiClient.post("/auth/refresh");
+
+        const newAccessToken = response.data.token;
+
+        localStorage.setItem("accessToken", newAccessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return apiClient(originalRequest);
+      } catch (refreshError) {
+        localStorage.removeItem("accessToken");
+
+        window.location.href = "/sign-in";
+
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default apiClient;
