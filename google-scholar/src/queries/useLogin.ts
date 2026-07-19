063e7f7fdada65e98f727e96d5cd16555da647fd
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-19 13:38:41 +0330

    feat(query): complete onSuccess of login hook

--- google-scholar/src/queries/useLogin.ts
+++ google-scholar/src/queries/useLogin.ts
@@ -1,11 +1,19 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import authService, { LoginRequest } from "../services/authService";
+import { useNavigate } from "react-router-dom";
 
 interface LoginResponse {
   token: string;
 }
 
-export const useLogin = () =>
-  useMutation<LoginResponse, Error, LoginRequest>({
+export const useLogin = () => {
+  const navigate = useNavigate();
+
+  return useMutation<LoginResponse, Error, LoginRequest>({
     mutationFn: authService.login,
+    onSuccess: (response: LoginResponse) => {
+      localStorage.setItem("accessToken", response.token);
+      navigate("/");
+    },
   });
+};
