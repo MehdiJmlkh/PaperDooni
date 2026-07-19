5e9c70be85e657f57a990097ff9ab1fbd79dbea6
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-19 12:11:14 +0330

    feat(ui): add error filed to PasswordInput

--- google-scholar/src/components/PasswordInput/PasswordInput.tsx
+++ google-scholar/src/components/PasswordInput/PasswordInput.tsx
@@ -4,28 +4,32 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { useState } from "react";
 import React from "react";
 
-interface Props extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: string;
+}
 
 const PasswordInput = React.forwardRef<HTMLInputElement, Props>(
-  ({ ...rest }, ref) => {
+  ({ error, ...rest }, ref) => {
     const [visible, setVisible] = useState(false);
 
     return (
-      <div className="password-input">
-        <input
-          className="form-control"
-          ref={ref}
-          {...rest}
-          type={visible ? "text" : "password"}
-        />
-
-        <span onClick={() => setVisible(!visible)}>
-          {visible ? (
-            <AiOutlineEye className="password-input__icon" />
-          ) : (
-            <AiOutlineEyeInvisible className="password-input__icon" />
-          )}
-        </span>
+      <div>
+        <div className="password-input">
+          <input
+            className={`form-control ${error ? "form-control--error" : ""}`}
+            ref={ref}
+            {...rest}
+            type={visible ? "text" : "password"}
+          />
+          <span onClick={() => setVisible(!visible)}>
+            {visible ? (
+              <AiOutlineEye className="password-input__icon" />
+            ) : (
+              <AiOutlineEyeInvisible className="password-input__icon" />
+            )}
+          </span>
+        </div>
+        {error && <p className="text-danger input__error">{error}</p>}
       </div>
     );
   },
