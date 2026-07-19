4b066b67566de5500fba907fd57e653531805468
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-19 09:08:27 +0330

    feat(ui): disable sign in button when the form is invalid

--- google-scholar/src/components/Form/Form.tsx
+++ google-scholar/src/components/Form/Form.tsx
@@ -1,12 +1,17 @@
-import { ReactNode } from "react";
+import { FormEvent, ReactNode } from "react";
 import "./Form.css";
 
 interface Props {
   children: ReactNode;
+  onSubmit?: (event: FormEvent) => void;
 }
 
-const Form = ({ children }: Props) => {
-  return <div className="form">{children}</div>;
+const Form = ({ children, onSubmit }: Props) => {
+  return (
+    <form className="form" onSubmit={onSubmit}>
+      {children}
+    </form>
+  );
 };
 
 export default Form;
--- google-scholar/src/pages/SignInPage/SignInPage.tsx
+++ google-scholar/src/pages/SignInPage/SignInPage.tsx
@@ -2,16 +2,34 @@ import Button from "../../components/Button";
 import Form from "../../components/Form";
 import Input from "../../components/Input";
 import Link from "../../components/Link";
+import { date, z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import PasswordInput from "../../components/PasswordInput/PasswordInput";
 import "./SignInPage.css";
+import { useForm } from "react-hook-form";
+
+const schema = z.object({
+  username: z.string().min(1),
+  password: z.string().min(6),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const SignInPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit((data) => console.log(data))}>
       <h1 className="form__heading">Sign in</h1>
-      <Input placeholder="Username" />
-      <PasswordInput placeholder="Password" />
-      <Button>Sign in</Button>
+      <Input {...register("username")} placeholder="Username" />
+      <PasswordInput {...register("password")} placeholder="Password" />
+      <Button disable={!isValid}>Sign in</Button>
       <div className="form__footer">
         <span>Not a member yet? </span>
         <Link to="/sign-up">Sign Up</Link>
