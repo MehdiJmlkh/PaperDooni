d91e92dce7241f6c970f8ff5f084e4ac5abb81b9
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-20 19:02:02 +0330

    feat(query): update query cache after editing user's phone number

--- google-scholar/src/queries/useEditPhoneNumber.ts
+++ google-scholar/src/queries/useEditPhoneNumber.ts
@@ -1,7 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
-import userService from "../services/userService";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import userService, { User } from "../services/userService";
 
-export const useEditPhoneNumber = () =>
-  useMutation<any, string, string>({
+export const useEditPhoneNumber = () => {
+  const queryClient = useQueryClient();
+  return useMutation<any, string, string>({
     mutationFn: userService.editPhoneNumber,
+    onSuccess: (_, newPhoneNumber) => {
+      queryClient.setQueriesData(["user"], (oldUser?: User) => {
+        if (!oldUser) return oldUser;
+
+        return { ...oldUser, phoneNumber: newPhoneNumber };
+      });
+    },
   });
+};
