bd64750c662da5e8a2aec856160c2bd4d3843812
Mehdi <mehdijmlkh1382@gmail.com>
2026-07-20 18:59:40 +0330

    feat(query): update query cache after editing user's email

--- google-scholar/src/queries/useEditEmail.ts
+++ google-scholar/src/queries/useEditEmail.ts
@@ -1,8 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
-import userService from "../services/userService";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import userService, { User } from "../services/userService";
 
 export const useEditEmail = () => {
+  const queryClient = useQueryClient();
   return useMutation<any, string, string>({
     mutationFn: userService.editEmail,
+    onSuccess: (_, newEmail) => {
+      queryClient.setQueriesData<User>(["user"], (oldUser?: User) => {
+        if (!oldUser) return oldUser;
+
+        return { ...oldUser, email: newEmail };
+      });
+    },
   });
 };
