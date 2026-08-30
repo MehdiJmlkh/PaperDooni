90e7358d8eb78d039f9348e9452f87c6251d853d
Mehdi <mehdijmlkh1382@gmail.com>
2026-08-30 14:46:43 +0330

    fix: remove unused secret generation

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -6,6 +6,5 @@ mkdir -p ./secrets
 
 openssl rand -base64 32 > ./secrets/db_root_password.txt
 openssl rand -base64 64 > ./secrets/jwt_secret.txt
-touch ./secrets/oauth_google_client_secret.txt
 
 echo "Secrets generated successfully."
